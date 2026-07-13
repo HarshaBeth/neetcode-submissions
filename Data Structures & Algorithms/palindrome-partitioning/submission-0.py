743b5f0b96e53b3e208bfcabf3dc29b0e0802852
Harsha B. Beth <92636321+HarshaBeth@users.noreply.github.com>
2026-07-13 10:55:51 +0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        partition = []
+
+        # i is the index of the character in the string
+        def dfs(i):
+            if i >= len(s):
+                res.append(partition.copy())
+                return
+            
+            for j in range(i, len(s)):
+                if self.isPalindrome(s, i, j):
+                    partition.append(s[i:j+1])
+                    dfs(j+1)
+                    partition.pop()
+        
+        dfs(0)
+        return res
+    
+    def isPalindrome(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        
+        return True
