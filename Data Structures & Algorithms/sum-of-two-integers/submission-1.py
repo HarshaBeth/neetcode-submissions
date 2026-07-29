f8b697265b08ff0e735dd0f8cde85cb5d28e95e8
Harsha B. Beth <92636321+HarshaBeth@users.noreply.github.com>
2026-07-29 23:38:04 +0400

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # while (b != 0):
+        #     tmp = (a & b) << 1
+        #     a = a ^ b
+        #     b = tmp
+        
+        # return a
+        return a + b
+
